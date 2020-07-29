2c90e2b3d4ed15653b82e4b5430a3e7bdf352ad1
AkrithiShetty <shettakrithi@gmail.com>
2020-07-29 10:28:27 +0530

    Commit Application

--- public/js/app.js
+++ public/js/app.js
@@ -6,21 +6,21 @@ const messageOne = document.querySelector('#message1');
 const messageTwo = document.querySelector('#message2');
 
 weatherAddress.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const location = search.value;
-  messageOne.textContent = 'Loading.....';
-  messageTwo.textContent = '';
-  fetch('/weather?address=' + location).then((res) => {
-    res.json().then((data) => {
-      messageOne.textContent = '';
+	e.preventDefault();
+	const location = search.value;
+	messageOne.textContent = 'Loading.....';
+	messageTwo.textContent = '';
+	fetch('/weather?address=' + location).then((res) => {
+		res.json().then((data) => {
+			messageOne.textContent = '';
 
-      if (data.error) {
-        messageTwo.textContent = data.error;
-        //return  console.log(data.error)
-      } else {
-        messageOne.textContent = data.location;
-        messageTwo.textContent = data.Forecast;
-      }
-    });
-  });
+			if (data.error) {
+				messageTwo.textContent = data.error;
+				//return  console.log(data.error)
+			} else {
+				messageOne.textContent = data.location;
+				messageTwo.textContent = data.Forecast;
+			}
+		});
+	});
 });
